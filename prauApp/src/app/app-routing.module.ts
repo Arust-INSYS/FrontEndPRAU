66e18bdd778c199f3719719643c97810eab1936c
wilmer <damian-019@hotmail.es>
2024-03-29 10:28:41 -0500

    sidenav

--- prauApp/src/app/app-routing.module.ts
+++ prauApp/src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 //MODULOS//
 import { LoginComponent } from './components/Login/Login.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: '**', redirectTo: 'login' },
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  //{ path: '**', redirectTo: 'login' },
+  //{ path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'sidenav', component: SidenavComponent}
 ];
 
 @NgModule({
---
+++ prauApp/src/app/components/sidenav/sidenav.component.ts
@@ -0,0 +1,18 @@
+import { Component, ViewChild } from '@angular/core';
+import { Sidebar } from 'primeng/sidebar';
+
+@Component({
+  selector: 'app-sidenav',
+  templateUrl: './sidenav.component.html',
+  styleUrl: './sidenav.component.css'
+})
+export class SidenavComponent {
+
+  @ViewChild('sidebarRef') sidebarRef!: Sidebar;
+
+    closeCallback(e:any): void {
+        this.sidebarRef.close(e);
+    }
+
+    sidebarVisible: boolean = false;
+}
