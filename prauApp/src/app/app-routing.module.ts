5f8c73e7e3b1c37a2889d3a0e6190aeb9d805995
Ariel-Aguirre-Soft <ariel.aguirre.est@tecazuay.edu.ec>
2024-03-30 10:53:15 -0500

    actualizacion front end
    delete side-nav

--- prauApp/src/app/app-routing.module.ts
+++ prauApp/src/app/app-routing.module.ts
@@ -6,7 +6,6 @@ import { LoginComponent } from './components/Login/Login.component';
 import { GestionarPersonaComponent } from './components/gestionar-persona/gestionar-persona.component';
 import { ListarPersonaComponent } from './components/gestionar-persona/listar-persona/listar-persona.component';
 import { RegistrarPersonaComponent } from './components/gestionar-persona/registrar-persona/registrar-persona.component';
-import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { MenuComponent } from './components/menu/menu.component';
 
 const routes: Routes = [
@@ -21,7 +20,6 @@ const routes: Routes = [
       { path: 'registrar', component: RegistrarPersonaComponent },
     ],
   },
-  { path: 'sidenav', component: SidenavComponent },
   { path: 'menu', component: MenuComponent },
 ];
 
