676a22fb16bed10d4a8cf7ff58ae2a19ffbef6ac
Ariel-Aguirre-Soft <ariel.aguirre.est@tecazuay.edu.ec>
2024-04-03 22:21:19 -0500

    Update periodoAc.ts

--- prauApp/src/app/models/periodoAc.ts
+++ prauApp/src/app/models/periodoAc.ts
@@ -1,23 +1,24 @@
-import { Aula } from './aula';
-
 export class PeriodoAc {
-  idPeriodoAc: number;
-  fechaInicio?: Date;
-  fechaFin?: Date;
-  nombrePeriodo: string;
-  descripcion: string;
-  aula?: Aula;
+    idPeriodoAc: number;
+    fechaInicio: Date;
+    fechaFin: Date;
+    nombrePeriodo: string;
+    descripcion: string;
+    aulas: any[];
 
-  constructor(
-    idPeriodoAc?: number,
-    fechaInicio?: Date,
-    fechaFin?: Date,
-    nombrePeriodo?: string,
-    descripcion?: string,
-    aula?: Aula
-  ) {
-    this.idPeriodoAc = idPeriodoAc || 0;
-    this.nombrePeriodo = nombrePeriodo || '';
-    this.descripcion = descripcion || '';
-  }
+    constructor(
+        idPeriodoAc?: number,
+        fechaInicio?: Date,
+        fechaFin?: Date,
+        nombrePeriodo?: string,
+        descripcion?: string,
+        aulas?: any[]
+    ) {
+        this.idPeriodoAc = idPeriodoAc || 0;
+        this.fechaInicio = fechaInicio || new Date();
+        this.fechaFin = fechaFin || new Date();
+        this.nombrePeriodo = nombrePeriodo || '';
+        this.descripcion = descripcion || '';
+        this.aulas = aulas || [];
+    }
 }
