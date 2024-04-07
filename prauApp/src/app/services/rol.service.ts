366cde3359b9561c3aec8da0302ea9e5cff23317
Ariel-Aguirre-Soft <ariel.aguirre.est@tecazuay.edu.ec>
2024-04-07 15:41:57 -0500

    Update rol.service.ts

--- prauApp/src/app/services/rol.service.ts
+++ prauApp/src/app/services/rol.service.ts
@@ -1,37 +1,36 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { LocalStorageService } from './local-storage.service'; // Importa localStorageService
 import { entorno } from '../env/entorno';
 import { Rol } from '../models/rol';
 import { Observable } from 'rxjs';
 
-
+//Cambios
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RolService {
+  constructor(
+    private http: HttpClient,
+    private localStorage: LocalStorageService
+  ) {}
 
-  constructor(private http: HttpClient, private localStorage: LocalStorageService) { }
-
-  private url: string = `${entorno.urlPrivada}/rol`
+  private url: string = `${entorno.urlPrivada}/rol`;
   // private token = this.localStorage.getItem('token');
-
 
   getAllRoles() {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.localStorage.getItem('token')}` // Agrega el token JWT aquí
+      Authorization: `Bearer ${this.localStorage.getItem('token')}`, // Agrega el token JWT aquí
     });
     // alert("GESTION= " + this.localStorage.getItem('token'))
 
     return this.http.get<Rol[]>(`${this.url}/read`, { headers });
-
   }
   createRol(rol: Rol): Observable<any> {
     const headers = new HttpHeaders({
-      'Authorization': `Bearer ${this.localStorage.getItem('token')}`
+      Authorization: `Bearer ${this.localStorage.getItem('token')}`,
     });
 
     return this.http.post<any>(`${this.url}/create`, rol, { headers });
   }
-
 }
