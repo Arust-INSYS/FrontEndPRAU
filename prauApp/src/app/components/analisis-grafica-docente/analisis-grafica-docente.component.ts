6a37adcd403a3b9335c9d717f7fa8e0ed50dc4c1
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2024-04-14 02:07:05 -0500

    Gráficas

--- prauApp/src/app/components/analisis-grafica-docente/analisis-grafica-docente.component.ts
+++ prauApp/src/app/components/analisis-grafica-docente/analisis-grafica-docente.component.ts
@@ -1,91 +1,138 @@
 import { Component, OnInit } from '@angular/core';
 import { DocenteService } from '../../services/docente.service';
+import { PeriodoAcService } from '../../services/periodo-ac.service';
+import { PeriodoAc } from '../../models/periodoAc';
+import { GraficaDocente } from '../../models/GraficaDocente';
+
 
 @Component({
   selector: 'app-analisis-grafica-docente',
   templateUrl: './analisis-grafica-docente.component.html',
-  styleUrl: './analisis-grafica-docente.component.css'
+  styleUrl: './analisis-grafica-docente.component.css',
 })
 export class AnalisisGraficaDocenteComponent implements OnInit {
   data: any;
-
   options: any;
-  constructor(private docenteService: DocenteService) { }
-  ngOnInit() {
-    this.mostrarDocente()
-      
-  }
-  mostrarDocente(){
-    this.docenteService.obtenerDatos(4, 1, 1, 1).subscribe(data => {
-      console.log("I HAVE THE POWER!!",data);
-      const documentStyle = getComputedStyle(document.documentElement);
-      const textColor = documentStyle.getPropertyValue('--text-color');
-      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
 
-      this.data = {
-          labels: ['Cumple', 'Cumple Medio', 'No Cumple'],
-          datasets: [
-              {
+  periodosAcademicos: any[] | undefined;
+  selectedPeriodoAc: string | undefined;
+  constructor(
+    private docenteService: DocenteService,
+    private periodoAcService: PeriodoAcService,
+    
+  ) {}
+  ngOnInit() {
+    this.mostrarDocente();
+    this.cargarPeriodos();
+    
+  }
+    //ARRAYS FILTROS
+    periodosAc: PeriodoAc[] = [];
+    cargarPeriodos(): void {
+      this.periodoAcService.getPeriodosAcs().subscribe((response) => {
+        this.periodosAc = response;
+        console.log("FILTROS", response)
+        // Limpiar el array antes de agregar nuevos elementos
+      this.periodosAcademicos = [];
+
+      // Iterar sobre los periodos y agregarlos a periodosAcademicos
+      for (const periodo of this.periodosAc) {
+        this.periodosAcademicos.push({ name: periodo.nombrePeriodo, code:periodo?.idPeriodoAc });
+      }
+        
+      });
+    }
+    codigoPeriodoAc:number=0;
+    cambioValor(event: any){
+        if (event && event.code) {
+            console.log("Cambié de valor:", event.code);
+            this.codigoPeriodoAc = event.code;
+          } else {
+            
+            this.codigoPeriodoAc = 0;
+            console.log("Evento indefinido. Asignando valor cero.",this.codigoPeriodoAc);
+          }
+    }
+dataDocente:any=[];
+
+  mostrarDocente() {
+    this.docenteService.obtenerDatos(0, 0, 0, this.codigoPeriodoAc).subscribe((data) => {
+        this.dataDocente=data;
+        this.dataDocente.forEach((docente:GraficaDocente) => {
+            console.log('I HAVE THE POWER!!', data);
+            const documentStyle = getComputedStyle(document.documentElement);
+            const textColor = documentStyle.getPropertyValue('--text-color');
+            const textColorSecondary = documentStyle.getPropertyValue(
+              '--text-color-secondary'
+            );
+            const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+      
+            this.data = {
+              labels: ['Cumple', 'Cumple Medio', 'No Cumple'],
+              datasets: [
+                {
                   type: 'bar',
                   label: 'C',
                   backgroundColor: documentStyle.getPropertyValue('--blue-500'),
-                  data: [data[0].porc_C]
-              },
-              {
+                  data: [docente.porc_C],
+                },
+                {
                   type: 'bar',
                   label: 'CM',
                   backgroundColor: documentStyle.getPropertyValue('--green-500'),
-                  data: [data[0].porc_CM,data[0].porc_CM]
-              },
-              {
+                  data: [docente.porc_CM, docente.porc_CM],
+                },
+                {
                   type: 'bar',
                   label: 'NC',
                   backgroundColor: documentStyle.getPropertyValue('--yellow-500'),
-                  data: [data[0].porc_NC,data[0].porc_NC,data[0].porc_NC]
-              }
-
-              
-          ]
-      };
-
-      this.options = {
-          maintainAspectRatio: false,
-          aspectRatio: 0.8,
-          plugins: {
-              tooltip: {
+                  data: [docente.porc_NC, docente.porc_NC, docente.porc_NC],
+                },
+              ],
+            };
+      
+            this.options = {
+              maintainAspectRatio: false,
+              aspectRatio: 0.8,
+              plugins: {
+                tooltip: {
                   mode: 'index',
-                  intersect: false
-              },
-              legend: {
+                  intersect: false,
+                },
+                legend: {
                   labels: {
-                      color: textColor
-                  }
-              }
-          },
-          scales: {
-              x: {
-                  stacked: true,
-                  ticks: {
-                      color: textColorSecondary
+                    color: textColor,
                   },
-                  grid: {
-                      color: surfaceBorder,
-                      drawBorder: false
-                  }
+                },
               },
-              y: {
+              scales: {
+                x: {
                   stacked: true,
                   ticks: {
-                      color: textColorSecondary
+                    color: textColorSecondary,
                   },
                   grid: {
-                      color: surfaceBorder,
-                      drawBorder: false
-                  }
-              }
-          }
-      };
+                    color: surfaceBorder,
+                    drawBorder: false,
+                  },
+                },
+                y: {
+                  stacked: true,
+                  ticks: {
+                    color: textColorSecondary,
+                  },
+                  grid: {
+                    color: surfaceBorder,
+                    drawBorder: false,
+                  },
+                },
+              },
+            };  
+
+
+            
+          });
+      
     });
   }
 }
