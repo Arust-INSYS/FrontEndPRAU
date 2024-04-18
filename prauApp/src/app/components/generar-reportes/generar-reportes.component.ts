21419079f5f31a6a07a8200b6f211f6a0434f96f
Arust <90620456+Arust-INSYS@users.noreply.github.com>
2024-04-18 02:39:35 -0500

    cambios evaluacion

--- prauApp/src/app/components/generar-reportes/generar-reportes.component.ts
+++ prauApp/src/app/components/generar-reportes/generar-reportes.component.ts
@@ -57,6 +57,7 @@ export class GenerarReportesComponent {
     this.loadCarreras();
     this.loadAsignaturas();
     this.loadCriterios();
+    this.graficaCiclosMaterias();
   }
 
   loadCarreras(): void {
@@ -245,6 +246,70 @@ export class GenerarReportesComponent {
           }
         }
       }
+    };
+  }
+  dataCicloMateria:any;
+  optionsCicloMateria:any;
+  graficaCiclosMaterias(){
+    console.log("CARGA DE DATOS CICLOS MATERIA")
+    const documentStyle = getComputedStyle(document.documentElement);
+    const textColor = documentStyle.getPropertyValue('--text-color');
+    const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+    const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+    this.dataCicloMateria = {
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+        datasets: [
+            {
+                label: 'My First dataset', 
+                backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+                borderColor: documentStyle.getPropertyValue('--blue-500'),
+                data: [65, 59, 80, 81, 56, 55, 40]
+            },
+            /* {
+                label: 'My Second dataset',
+                backgroundColor: documentStyle.getPropertyValue('--blue-500'),
+                borderColor: documentStyle.getPropertyValue('--blue-500'),
+                data: [28, 48, 40, 19, 86, 27, 90]
+            } */
+        ]
+    };
+    
+
+    this.optionsCicloMateria = {
+        indexAxis: 'y',
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+            legend: {
+                labels: {
+                    color: textColor
+                }
+            }
+        },
+        scales: {
+            x: {
+                ticks: {
+                    color: textColorSecondary,
+                    font: {
+                        weight: 500
+                    }
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            },
+            y: {
+                ticks: {
+                    color: textColorSecondary
+                },
+                grid: {
+                    color: surfaceBorder,
+                    drawBorder: false
+                }
+            }
+        }
     };
   }
  
